5a32a9ff8b76d1680e85d3208571feb0327d8a1e
shreyasintax <118666605+shreyasintax@users.noreply.github.com>
2024-03-08 18:31:54 +0530

    added onboarding

--- Frontend/hackmate-frontend/src/App.js
+++ Frontend/hackmate-frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { Registration } from './components/component/registration';
+import { Onboarding } from './components/component/onboarding';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Registration/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Registration />} />
+        <Route path="/about" element={<Onboarding />} />
+      </Routes>
+    </Router>
   );
 }
 
