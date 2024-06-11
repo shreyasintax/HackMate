8377a834ff71451ae0c2a81b9a5d8217a161038d
shreyasintax <shreyasinha101.ss@gmail.com>
2024-06-11 12:20:56 +0530

    added logout route from backend

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getSingleUser, deleteProfile, addUser } = require("../controllers/Profile");
-const { login, sendOTP, verifyOtp, changePassword, contactUs } = require("../controllers/Auth");
+const { login, sendOTP, verifyOtp, changePassword, contactUs, logout } = require("../controllers/Auth");
 const { auth } = require("../middlewares/auth");
 const { isOtpVerified } = require("../middlewares/verifyOTP");
 const { resetPasswordToken, resetPassword } = require("../controllers/ResetPassword.js");
@@ -11,6 +11,7 @@ router.get("/", auth, getSingleUser);
 
 router.post("/signup", isOtpVerified, addUser);
 router.post("/login", login);
+router.post("/logout", logout);
 router.post("/changepassword", auth, changePassword);
 
 router.post("/sendOtp", sendOTP);
